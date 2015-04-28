66c81916bec517e2075c3a0326843f33654bb9c7
jingxi1011 <angella1011@gmail.com>
2015-04-28 23:29:38 +0200

    Jingxi Xu 113867 Zhixing Hu113963

--- BrokenProject/app/src/main/java/mmbuw/com/brokenproject/AnotherBrokenActivity.java
+++ BrokenProject/app/src/main/java/mmbuw/com/brokenproject/AnotherBrokenActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
 import mmbuw.com.brokenproject.R;
 
 public class AnotherBrokenActivity extends Activity {
